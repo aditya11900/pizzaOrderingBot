ab297583db42231f7056b0ad133a6f0ff47a552c
aditya raj <adityaraj153566@gmail.com>
2024-04-27 13:15:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 import openai
+from dotenv import load_dotenv, find_dotenv
 import panel as pn
+_ = load_dotenv(find_dotenv()) # read local .env file
 
-
-openai.api_key  = 'api-key'
+openai.api_key  = 'api_key_here'
 
 
 def get_completion(prompt, model="gpt-3.5-turbo"):
@@ -11,7 +12,7 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
-        temperature=0, 
+        temperature=0, # this is the degree of randomness of the model's output
     )
     return response.choices[0].message["content"]
 
@@ -19,12 +20,12 @@ def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0)
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
-        temperature=temperature, 
+        temperature=temperature, # this is the degree of randomness of the model's output
     )
 #     print(str(response.choices[0].message))
     return response.choices[0].message["content"]
 
-inp = ...
+inp = ...  # Define the variable "inp" here
 context = [ {'role':'system', 'content':"""
 You are OrderBot, an automated service to collect orders for a pizza restaurant. \
 You first greet the customer, then collects the order, \
@@ -32,6 +33,7 @@ and then asks if it's a pickup or delivery. \
 You wait to collect the entire order, then summarize it and check for a final \
 time if the customer wants to add anything else. \
 If it's a delivery, you ask for an address. \
+make the payment in indian Rupees. \
 Finally you collect the payment.\
 Make sure to clarify all options, extras and sizes to uniquely \
 identify the item from the menu.\
@@ -54,7 +56,7 @@ coke 3.00, 2.00, 1.00 \
 sprite 3.00, 2.00, 1.00 \
 bottled water 5.00 \
 """} ]
-panels = [] 
+panels = []  # Define the variable "panels" here
 def collect_messages(_):
     prompt = inp.value_input
     inp.value = ''
@@ -85,12 +87,3 @@ dashboard = pn.Column(
 )
 
 dashboard
-
-messages =  context.copy()
-messages.append(
-{'role':'system', 'content':'create a json summary of the previous food order. Itemize the price for each item\
- The fields should be 1) pizza, include size 2) list of toppings 3) list of drinks, include size   4) list of sides include size  5)total price '},    
-)
-    
-response = get_completion_from_messages(messages, temperature=0)
-print(response)
